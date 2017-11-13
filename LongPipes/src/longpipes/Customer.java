43b36220d73261e850aaddada2fa6e29cc1aec6b
Davebuck98 <33024481+Davebuck98@users.noreply.github.com>
2017-11-13 20:35:16 +0000

    Update customer.java
    Class for the customer

--- LongPipes/src/longpipes/Customer.java
+++ LongPipes/src/longpipes/Customer.java
@@ -11,4 +11,87 @@ package longpipes;
  */
 public class Customer {
     
+    private String firstName, surName, email, phone, company, companyAddress;
+    private double payment;
+    
+    public Customer()
+    {}
+    
+    public Customer(String cFName, String cSName, String cEmail, String cPhone, String cCompany, String cAddress, double cPayment){
+    
+        firstName = cFName;
+        surName = cSName;
+        email = cEmail;
+        phone = cPhone;
+        company = cCompany;
+        companyAddress = cAddress;
+        payment = cPayment;
+    }
+    
+    public void setFName(String fNameIn){
+        firstName = fNameIn;    
+    }
+    
+    public String getFName(){
+        return firstName;
+    }
+    
+    public void setSName(String sNameIn){
+        surName = sNameIn;    
+    }
+    
+    public String getSName(){
+        return surName;
+    }
+    
+    public void setEmail(String emailIn){
+        email = emailIn;    
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setPhone(String phoneIn){
+        phone = phoneIn;    
+    }
+    
+    public String getPhone(){
+        return phone;
+    }
+    
+    public void setCompany(String companyIn){
+        company = companyIn;    
+    }
+    
+    public String getCompany(){
+        return company;
+    }
+    
+    public void setCompAdd(String companyAddIn){
+        company = companyAddIn;    
+    }
+    
+    public String getCompAdd(){
+        return company;
+    }
+    
+    public void setPayment(double paymentIn){
+        payment = paymentIn;    
+    }
+    
+    public double getPayment(){
+        return payment;
+    }
+    
+    // Displays the details of the customer
+    public void displayDetails(){
+        System.out.println("\nName: " + firstName + " " + surName);
+        System.out.println("\nEmail Address: " + email);
+        System.out.println("\nPhone Number: " + phone);
+        System.out.println("\nCompany Name: " + company);
+        System.out.println("\nCompany Address: " + companyAddress);
+        System.out.println("\nAmount Paid: £" + payment);
+    }
+            
 }
