b8bcc11f5fd23ffedffb412e80e1862a61a077e9
Nasser333 <33520475+Nasser333@users.noreply.github.com>
2017-11-15 20:20:07 +0000

    Update Order.java

--- LongPipes/src/longpipes/Order.java
+++ LongPipes/src/longpipes/Order.java
@@ -7,8 +7,55 @@ package longpipes;
 
 /**
  *
- * @author User
+ * @author Nasser 
  */
+
 public class Order {
     
+    private int customerID;
+    private int orderID;    
+    private int orderDate;
+    private float orderTotal; 
+    private ArrayList<PipeOrder> pipeOrders;
+
+    public Order(int stCustomerID, int stOrderID, int stOrderDate, float stOrderTotal) {
+       customerID = stCustomerID;
+       orderID = stOrderID;
+       orderDate = stOrderDate;
+       orderTotal = 0;
+       pipeOrders = new ArrayList<Piporder>();  
+    }
+   
+    
+    public void setCustomerID(int customerIDln){
+        customerID = customerIDln;
+      }
+    
+    public int getCustomerID(){
+        return customerID;
+    }
+            
+    public void setOrderID(int orderIDln){
+        orderID = orderIDln;
+    } 
+    
+    public int getOrderID(){
+        return orderID;
+    }
+     
+    public void setOrderDate(int orderDateln){
+        orderDate = orderDateln;
+    }
+    
+    public int getOrderDate(){
+        return orderDate;
+    }
+            
+ // Assuming that Pipeorder class will have "getCost"                       
+            
+    public float orderTotal() {
+        for (Pipeorder aPipeOrder : pipeOrders) 
+               orderTotal += aPipeOrder.getCost();
+    } 
 }
+
